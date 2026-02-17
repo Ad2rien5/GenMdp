3294195f6112c062d5d83f9c36b9bbe9bb3f0a91
Adrien SEAC'H <62133470+Ad2rien5@users.noreply.github.com>
2026-02-17 13:36:41 +0100

    Add password size input and validation in start.bat

--- src/start.bat
+++ src/start.bat
@@ -1,1 +1,28 @@
-:: 
+@echo off
+
+:: Size of the password
+set /p "nombre=Which size should be this new password : "
+
+for /f "delims=0123456789" %%a in ("%nombre%") do (
+    echo Your input is not a valid number, try again.
+    goto demande
+)
+
+:: Specification of the new password
+:: Uppercase
+
+:: Number
+
+:: Special character
+
+:: Function
+: IsBool
+if /i %1=="y" (
+    set bool=true
+) else if /i %1=="n" (
+    echo Vous avez choisi : Non
+    set bool=false
+) else (
+    echo Your input is not a valid answer, try again.
+    goto %2
+)
