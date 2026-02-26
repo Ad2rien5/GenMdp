046d5e522ec3caf227d7e651a5946eb466e77396
Adrien SEAC'H <62133470+Ad2rien5@users.noreply.github.com>
2026-02-26 10:17:59 +0100

    Finish all setter for the parameter of the password

--- src/start.bat
+++ src/start.bat
@@ -4,9 +4,10 @@
 : IsBool
 if /i %test=="y" (
     set bool=true
+    goto %next%
 ) else if /i %test=="n" (
     set bool=false
-    goto %
+    goto %next%
 
 ) else (
     echo Your input is not a valid answer, try again.
@@ -27,13 +28,24 @@ for /f "delims=0123456789" %%a in ("%nombre%") do (
 set actual=Uppercase
 set next=Number
 
+set "test=Are uppercase allowed?(y/n) "
+goto IsBool
+
 : Number
+set uppercase=%bool%
 set actual=Number
 set next=SpecialChar
 
+set "test=Are number allowed?(y/n) "
+goto IsBool
+
 : SpecialChar
+set number=%bool%
 set actual=SpecialChar
 set next=END
+
+set "test=Are spécial character allowed?(y/n) "
+goto IsBool
 
 :: END
 : END
